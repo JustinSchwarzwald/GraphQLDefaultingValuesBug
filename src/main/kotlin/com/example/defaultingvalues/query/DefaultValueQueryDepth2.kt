3bef5756aa3a48474c93d0c605fb262af1ed5d67
Justin Schwarzwald <43459254+JustinSchwarzwald@users.noreply.github.com>
2022-08-02 16:25:11 -0700

    Update DefaultValueQueryDepth2.kt

--- src/main/kotlin/com/example/defaultingvalues/query/DefaultValueQueryDepth2.kt
+++ src/main/kotlin/com/example/defaultingvalues/query/DefaultValueQueryDepth2.kt
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component
 
 @Component
 class DefaultValueQueryDepth2 : Query {
-    suspend fun returnTrueIfValueDefaults(parentQueryModel: ParentQueryModel): Boolean {
+    suspend fun returnTrueIfChildValueDefaults(parentQueryModel: ParentQueryModel): Boolean {
         return parentQueryModel.childQueryModel?.value == "defaultValue"
     }
 }
